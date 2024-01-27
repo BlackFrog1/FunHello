cbd3cb4fb4e1d95fe8165e39e84c94f85ea2576a
BlackFrog <blackfrog1@gmail.com>
2024-01-27 11:22:33 -0500

    Initial Commit

---
+++ FunHello/FunHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunHelloApp.swift
+//  FunHello
+//
+//  Created by Collin Bennett on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FunHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
