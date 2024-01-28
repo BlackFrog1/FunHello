c5ae28bde3b7fe5b3fa24cf0a8b1d6aa50d4dd2f
BlackFrog <blackfrog1@gmail.com>
2024-01-27 21:31:31 -0500

    Put card viiew in it's own file

--- FunHello/CardView.swift
+++ FunHello/CardView.swift
@@ -5,4 +5,24 @@
 //  Created by Collin Bennett on 1/27/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CardView: View {
+    let icon: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(icon).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
